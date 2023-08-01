0b8c902c5836ea7355dd1a9d4d1960c6869d9cfe
kimjongha99 <jjhsun99@gmail.com>
2023-08-01 11:55:00 +0900

    로그추가

--- w1/src/main/java/org/zero/jdbcex/controller/TodoListController.java
+++ w1/src/main/java/org/zero/jdbcex/controller/TodoListController.java
@@ -14,10 +14,16 @@ import java.util.List;
 @Log4j2
 public class TodoListController extends HttpServlet {
     private TodoService todoService = TodoService.INSTANCE;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         log.info("todo list..................");
+
+        ServletContext servletContext = req.getServletContext();
+
+        log.info("appName:  "  + servletContext.getAttribute("appName"));
+
 
         try {
             List<TodoDTO> dtoList = todoService.listAll();
